46060b1b9d0e5222b637f7a9e4c6f20a51bd74d2
onassar <onassar@gmail.com>
2018-11-28 20:55:38 -0500

    Init

---
+++ LocationSniffer.php
@@ -0,0 +1,12 @@
+<?php
+
+    /**
+     * LocationSniffer
+     * 
+     * @abstract
+     * @link    https://github.com/getstencil/PHP-LocationSniffer
+     * @author  Oliver Nassar <oliver@getstencil.com>
+     */
+    abstract class LocationSniffer
+    {
+    }
